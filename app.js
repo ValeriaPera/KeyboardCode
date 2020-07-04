8ea876ab78fcccdd5f4dda8f3deb3fcbd8918e5c
Valeria <valeriaapera@gmail.com>
2020-07-04 02:43:52 -0300

    diseño

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const key = document.querySelector('kbd')
-const keycode = document.querySelector('h2')
+const keycode = document.querySelector('#keycode')
 
 document.addEventListener('keydown', e => {
   if (e.keyCode === 32) {
